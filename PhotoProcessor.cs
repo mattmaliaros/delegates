9381297e874d540c8a63bbe78d99be6d9690080b
Matt <mattmaliaros@gmail.com>
2019-11-20 15:49:06 -0500

    Add project files.

---
+++ PhotoProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class PhotoProcessor
+    {
+        public delegate void PhotoFilterHandler(Photo photo);
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+
+            //Delegates
+            //System.Action<>
+            //System.Func<>;
+
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+            //var filters = new PhotoFilters();
+            //filters.ApplyBrightness(photo);
+            //filters.ApplyContrast(photo);
+            //filters.Resize(photo);
+            photo.Save();
+        }
+    }
+}
